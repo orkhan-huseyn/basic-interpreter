a41c8612754ac778f3b6688eef76faaebf8f7e05
Orxan Hüseynli <o.huseynli@asan.local>
2017-12-04 16:25:17 +0400

    writing parse

--- src/com/csci/parser/Parser.java
+++ src/com/csci/parser/Parser.java
@@ -1,54 +1,30 @@
 package com.csci.parser;
 
-import com.csci.components.stms.Stm;
 import com.csci.lexer.Token;
 import com.csci.lexer.TokenType;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public class Parser {
 
-    private List<Token> tokens;
-    private Token next;
-    private int index;
+    LinkedList<Token> tokens;
+    Token lookahead;
 
-    public Parser(List<Token> tokens) {
+    public void parse(LinkedList<Token> tokens) {
+
         this.tokens = tokens;
-        this.index = 0;
-        this.next = tokens.get(index);
+        lookahead = this.tokens.getFirst();
+
+        expression();
+
     }
 
-    public Stm parse() {
+    private void expression() {
 
-        Token first  = next;
-        Token second = tokens.get(1);
-
-        if (first.getType().equals(TokenType.TYPEINT) && second.getType().equals(TokenType.IDENT)) {
-            parseDeclaration();
-        }
-
-        return null;
     }
 
-    public Stm parseDeclaration() {
-
-        return null;
-
-    };
-
-    public void nextToken(Token expected) throws Exception {
-        if (next.getData().equals(expected.getData())) {
-            next();
-        }
-        else {
-            throw new Exception(String.format("Expected %s but got %s", expected.getData(), next.getData()));
-        }
-    }
-
-    private void next() {
-        if (index < tokens.size()) {
-            index++;
-            next = tokens.get(index);
-        }
+    private void nextToken() {
+        tokens.pop();
+        lookahead = tokens.getFirst();
     }
 }
