72eafca2e6177e875498759ab393490b9846482f
Orxan Hüseynli <o.huseynli@asan.local>
2017-12-29 18:36:55 +0400

    custom object created

---
+++ src/com/csci/grammar/CustomObject.java
@@ -0,0 +1,14 @@
+package com.csci.grammar;
+
+public class CustomObject {
+
+    public Type type;
+
+    public Object value;
+
+    public CustomObject(Type type, Object value) {
+        this.type = type;
+        this.value = value;
+    }
+
+}
